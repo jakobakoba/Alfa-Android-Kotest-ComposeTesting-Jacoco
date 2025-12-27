1a45179117f5f1670107778cf87db8c43503e198
jakobakoba <borisgabyshev96dev@gmail.com>
2025-12-27 15:43:19 +0900

    feat: дата сорсы в один файл

--- app/src/main/java/com/bor96dev/hwflow/data/DataSource.kt
+++ app/src/main/java/com/bor96dev/hwflow/data/DataSource.kt
@@ -1,7 +1,30 @@
 package com.bor96dev.hwflow.data
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 interface DataSource {
     fun getData(): Flow<List<String>>
+}
+
+class AsiaDataSource : DataSource {
+    override fun getData(): Flow<List<String>> = flow {
+        delay(2000)
+        emit(listOf("Tokyo", "Beijing", "Seoul"))
+    }
+}
+
+class EuropeDataSource : DataSource {
+    override fun getData(): Flow<List<String>> = flow {
+        delay(1500)
+        emit(listOf("Paris", "London", "Berlin"))
+    }
+}
+
+class NorthAmericaDataSource : DataSource {
+    override fun getData(): Flow<List<String>> = flow {
+        delay(1000)
+        emit(listOf("New York", "Toronto", "Los Angeles"))
+    }
 }
